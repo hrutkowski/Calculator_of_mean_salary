863a81b61720f7fc65f8c14788736a9bb9a97ec9
Hubert Rutkowski <hubert.rutkowski3.stud@pw.edu.pl>
2021-01-19 10:28:40 +0100

    Beginning

---
+++ zadanie_algorytmy.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+class Employee
+{
+public:
+    Employee(std::string name, float salary, bool isMale)
+    : name_(name), salary_(salary), isMale_(isMale) {}
+
+    Employee() = default;
+
+    auto name() const { return name_;}
+    auto salary() const {return salary_;}
+    auto isMale() const {return isMale_;}
+
+private:
+    std::string name_;
+    float salary_;
+    bool isMale_;
+};
+
+auto getEmployees()
+{
+    auto result = std::vector<Employee>();
+
+    result.emplace_back("Jan Niezbędny", 20000, true);
+    result.emplace_back("Fryderyk Niekonieczny", 3200, true);
+    result.emplace_back("Zofia Przydatna", 8400, false);
+    result.emplace_back("Anna Pracowita", 12500, false);
+    result.emplace_back("Krzysztof Leń", 1500, true);
+    result.emplace_back("Zygmunt Koleżeński", 6100, true);
+    result.emplace_back("Renata Solidna", 11600, false);
+
+    return result;
+}
+
+
+int main()
+{
+    std::cout << "PROE lab14 - zadanie oceniane 3 \n";
+
+    auto employees = getEmployees();
+
+    // std::cout << "Średnia zarobków wszystkich pracowników wynosi : " << calculateMean(employees) << "\n";
+    // std::cout << "Średnia zarobków mężczyzn wynosi : " << calculateMaleMean(employees) << "\n";
+    // std::cout << "Średnia zarobków kobiet wynosi : " << calculateFemaleMean(employees) << "\n";
+
+
+    return 0;
+}
